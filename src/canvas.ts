a5716dc7caf88b18d8a46682fc8aa3fca29cc0bf
christopher-besch <mail@chris-besch.com>
2022-03-01 12:05:09 +0100

    added green square

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,8 +1,14 @@
+export type Ctx = CanvasRenderingContext2D;
+
 export function setup_canvas(): CanvasRenderingContext2D {
     let canvas = document.getElementById("play_canvas") as HTMLCanvasElement;
     canvas.width = canvas.scrollWidth;
     canvas.height = canvas.scrollHeight;
     let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    console.log("Hello World!");
     return ctx;
+}
+
+export function draw_quad(ctx: Ctx): void {
+    ctx.fillStyle = "green";
+    ctx.fillRect(50, 50, 100, 100);
 }
