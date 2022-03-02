a96734e379d288501cdb8b280706be4d68722046
waveby <waving.by@gmail.com>
2022-03-02 09:25:36 +0100

    playerMovement

---
+++ src/game/player.ts
@@ -0,0 +1,39 @@
+// control player
+import {g_shapes, start} from "../engine/engine";
+import {Shape, Circle, Rectangle} from "../engine/shapes";
+
+// g_shapes.push(new Circle(ctx, 150, 50, "red", 1, "blue", 100));
+
+export class Player {
+    private m_circle: Circle;
+    private m_x: number;
+    private m_y: number;
+
+    constructor(x: number, y: number) {
+        this.m_circle = new Circle(x, y, "red", 1, "blue", 100);
+        this.m_x = x;
+        this.m_y = y;
+        g_shapes.push(this.m_circle);
+    }
+
+    public update() {
+        document.addEventListener("keydown", (event) => {
+            if (event.key === "d") {
+                this.m_x += 0.1;
+                this.m_circle.set_location(this.m_x, this.m_y);
+            }
+            if (event.key === "a") {
+                this.m_x += -0.1;
+                this.m_circle.set_location(this.m_x, this.m_y);
+            }
+            if (event.key === "s") {
+                this.m_y += 0.1;
+                this.m_circle.set_location(this.m_x, this.m_y);
+            }
+            if (event.key === "w") {
+                this.m_y += -0.1;
+                this.m_circle.set_location(this.m_x, this.m_y);
+            }
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,10 @@
-import { g_shapes, start } from "./engine/engine";
-import { Shape, Circle, Rectangle } from "./engine/shapes";
+import {g_shapes, start} from "./engine/engine";
+import {Shape, Circle, Rectangle} from "./engine/shapes";
 
 // g_shapes.push(new Circle(ctx, 150, 50, "red", 1, "blue", 100));
-
-class Player {
-    private m_circle: Circle;
-    private m_x: number;
-    private m_y: number;
-
-    constructor(x: number, y: number) {
-        this.m_circle = new Circle(x, y, "red", 1, "blue", 100);
-        this.m_x = x;
-        this.m_y = y;
-        g_shapes.push(this.m_circle);
-    }
-
-    public move() {
-        this.m_x += 0.1;
-        this.m_circle.set_location(this.m_x, this.m_y);
-    }
-}
-
+import {Player} from "./game/player";
 let p = new Player(100, 100);
 
 start(() => {
-    p.move();
+    p.update();
 });
