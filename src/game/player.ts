5e877cce24bd55e5b2ba7ddf171da2ee2058c588
waveby <waving.by@gmail.com>
2022-03-02 11:07:49 +0100

    speed

--- src/game/player.ts
+++ src/game/player.ts
@@ -3,6 +3,7 @@ import {g_shapes, start} from "../engine/engine";
 import {Shape, Circle, Rectangle} from "../engine/shapes";
 
 // g_shapes.push(new Circle(ctx, 150, 50, "red", 1, "blue", 100));
+const speed = "0";
 
 export class Player {
     private m_circle: Circle;
@@ -11,27 +12,26 @@ export class Player {
 
     constructor(x: number, y: number) {
         this.m_circle = new Circle(x, y, "red", 1, "blue", 100);
-        this.m_x = x;
-        this.m_y = y;
+        this.m_x = 0;
+        this.m_y = 0;
         g_shapes.push(this.m_circle);
     }
-
     public update() {
         document.addEventListener("keydown", (event) => {
             if (event.key === "d") {
-                this.m_x += 0.1;
+                this.m_x = 0.1 + +speed;
                 this.m_circle.set_location(this.m_x, this.m_y);
             }
             if (event.key === "a") {
-                this.m_x += -0.1;
+                this.m_x = -0.1 + +speed;
                 this.m_circle.set_location(this.m_x, this.m_y);
             }
             if (event.key === "s") {
-                this.m_y += 0.1;
+                this.m_y = -0.1 + +speed;
                 this.m_circle.set_location(this.m_x, this.m_y);
             }
             if (event.key === "w") {
-                this.m_y += -0.1;
+                this.m_y = 0.1 + +speed;
                 this.m_circle.set_location(this.m_x, this.m_y);
             }
         });
