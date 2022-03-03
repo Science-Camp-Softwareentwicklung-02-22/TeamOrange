58fc8906f0e6e32abf2a094088879fa22adff04e
christopher-besch <mail@chris-besch.com>
2022-03-03 11:04:32 +0100

    fixed player movement...sorta

--- src/game/player.ts
+++ src/game/player.ts
@@ -1,107 +1,100 @@
 // control player
-
-import {Circle, Rectangle} from "../engine/shapes";
-import {Renderer} from "../engine/renderer";
+import { Circle, Rectangle } from "../engine/shapes";
+import { Renderer } from "../engine/renderer";
 import * as math from "mathjs";
-import {boolean, e, subtract} from "mathjs";
 
-// gucken was passiert wenn knopof gedrückt ist; w
 export class Player {
     private m_renderer: Renderer;
 
     private m_pos: math.Matrix;
-    private m_vel: math.Matrix;
+    // pixel per frame
+    private m_vel: math.Matrix = math.matrix([0, 0]);
+    // pixel per frame per frame
+    private m_acc: number = 5;
 
     private m_circle: Circle;
-    private m_acc: number = 100;
 
+    // directional vectors
     private m_up: math.Matrix = math.matrix([0, -1]);
     private m_down: math.Matrix = math.matrix([0, 1]);
     private m_right: math.Matrix = math.matrix([1, 0]);
     private m_left: math.Matrix = math.matrix([-1, 0]);
 
     constructor(renderer: Renderer, pos: math.Matrix) {
-        this.m_pos = math.matrix([]);
-        // this.m_pos = pos
-        this.m_vel = math.matrix([0, 0]);
-        this.m_circle = new Circle(this.m_pos, "red", 1, "blue", 1, 100);
         this.m_renderer = renderer;
+        this.m_pos = pos;
+        this.m_circle = new Circle(this.m_pos, "red", 1, "blue", 1, 50);
         this.m_renderer.add_shape(this.m_circle);
 
-        // switch
-        // m.pos zu m. vel
-        // unterer bereich statt add substrakt
-        // update --> m pos zu set pos
-
+        // increase velocity when pressing
         document.addEventListener("keydown", (event) => {
-            switch (event.key) {
-                case "d":
+            switch (event.code) {
+                case "KeyD":
                     this.m_vel = math.add(
                         this.m_vel,
                         math.multiply(this.m_right, this.m_acc)
                     ) as math.Matrix;
                     break;
-                //this.m_circle.set_pos(this.m_pos);
-                case "a": {
+                case "KeyA": {
                     this.m_vel = math.add(
                         this.m_vel,
                         math.multiply(this.m_left, this.m_acc)
                     ) as math.Matrix;
                     break;
-                    //this.m_circle.set_pos(this.m_pos);
                 }
-                case "s": {
+                case "KeyS": {
                     this.m_vel = math.add(
                         this.m_vel,
                         math.multiply(this.m_down, this.m_acc)
                     ) as math.Matrix;
                     break;
-                    //this.m_circle.set_pos(this.m_pos);
                 }
-                case "w": {
+                case "KeyW": {
                     this.m_vel = math.add(
                         this.m_vel,
                         math.multiply(this.m_up, this.m_acc)
                     ) as math.Matrix;
-                    //this.m_circle.set_pos(this.m_pos);
                     break;
                 }
             }
-            document.addEventListener("keyup", (event) => {
-                switch (event.key) {
-                    case "w":
-                        this.m_vel = math.subtract(
-                            math.multiply(this.m_up, this.m_acc),
-                            this.m_vel
-                        ) as math.Matrix;
-                        break;
-                    case "a":
-                        this.m_vel = math.subtract(
-                            math.multiply(this.m_left, this.m_acc),
-                            this.m_vel
-                        ) as math.Matrix;
-                        break;
-                    case "s":
-                        this.m_vel = math.subtract(
-                            math.multiply(this.m_down, this.m_acc),
-                            this.m_vel
-                        ) as math.Matrix;
-                        break;
-                    case "d":
-                        this.m_vel = math.subtract(
-                            math.multiply(this.m_right, this.m_acc),
-                            this.m_vel
-                        ) as math.Matrix;
-                        break;
+        });
+
+        document.addEventListener("keyup", (event) => {
+            // decrease velocity when releasing
+            switch (event.code) {
+                case "KeyD":
+                    this.m_vel = math.subtract(
+                        this.m_vel,
+                        math.multiply(this.m_right, this.m_acc)
+                    ) as math.Matrix;
+                    break;
+                case "KeyA": {
+                    this.m_vel = math.subtract(
+                        this.m_vel,
+                        math.multiply(this.m_left, this.m_acc)
+                    ) as math.Matrix;
+                    break;
                 }
-                //Bullets
-                //
-                //
-                //
-            });
+                case "KeyS": {
+                    this.m_vel = math.subtract(
+                        this.m_vel,
+                        math.multiply(this.m_down, this.m_acc)
+                    ) as math.Matrix;
+                    break;
+                }
+                case "KeyW": {
+                    this.m_vel = math.subtract(
+                        this.m_vel,
+                        math.multiply(this.m_up, this.m_acc)
+                    ) as math.Matrix;
+                    break;
+                }
+            }
         });
     }
+
     public update() {
+        // TODO: use timings
         this.m_pos = math.add(this.m_pos, this.m_vel) as math.Matrix;
         this.m_circle.set_pos(this.m_pos);
     }
